020ac2db2a619665b651182951df3780cdde1821
E-Nincy <escotonincy10@outlook.es>
2025-10-23 09:17:56 +0200

    create and completed 00-intro.js file and completed 01.js exercise

--- 05-collections_arrays_objects/01.js
+++ 05-collections_arrays_objects/01.js
@@ -17,3 +17,71 @@
  *    function that takes an array as an argument and returns the sum. Use this
  *    function to log the sum of the two arrays of odd and even numbers.
  */
+
+
+/** --Array with obejcts in my room-- */
+let itemsInRoom = ["chair", "keyboard", "monitor", "desk", "lamp"];
+
+/** --Array with sub-arrays */
+let boxes = [
+    ["notebook", "pen", "stapler", "paper clips", "tape"],
+    ["socks", "t-shirt", "jeans", "jacket", "hat"],
+    ["book", "usb", "watch", "eraser", "earphones"]
+];
+
+/** --5 elements using referencies */
+console.log(boxes[0][1]); // pen
+console.log(boxes[1][3]); // jacket
+console.log(boxes[2][0]); // book
+console.log(boxes[0][4]); // tape
+console.log(boxes[1][0]); // socks
+
+/** --Array of numbers from 1 to 100-- */
+let numbers = [];
+
+for (let i = 1; i <= 100; i++) {
+    numbers.push(i);
+}
+
+/** --Sum of all numbers from 1 to 100-- */
+let sum = 0;
+for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+}
+
+console.log("Sum of numbers from 1 to 100:", sum);
+
+/** --Array of 100 random numbers-- */
+function getRandomNumber(){
+    return Math.floor((Math.random() * 100) + 1);
+}
+
+let randomNumbers = [];
+for (let i = 0; i < 100; i++) {
+    randomNumbers.push(getRandomNumber());
+}
+
+/** --Separate odd and even numbers-- */
+let oddNumbers = [];
+let evenNumbers = [];
+
+for (let i = 0; i < randomNumbers.length; i++) {
+    if (randomNumbers[i] % 2 === 0) {
+        evenNumbers.push(randomNumbers[i]);
+    } else {
+        oddNumbers.push(randomNumbers[i]);
+    }
+}
+
+/** --Function to sum arrays-- */
+function sumArray(array) {
+    let total = 0;
+    for (let i = 0; i < array.length; i++) {
+        total += array[i];
+    }
+    return total;
+}
+
+console.log("Sum of odd numbers:", sumArray(oddNumbers));
+console.log("Sum of even numbers:", sumArray(evenNumbers));
+
